dd272dd979b340d5c8f3ef201a69c163c78b71fe
DanFerrer <danielferrer212@gmail.com>
2014-07-22 17:05:17 -0400

    7/22/14 -  Rewrote model data and calculate service in seperate files

---
+++ app/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Daniel on 7/9/2014.
+ */
+
+var oneRepMax = angular.module('oneRepMax', []).constant('validation',  {
+        title: 'Angular 1RM Calculator',
+        MIN_REPS: 1,
+        MAX_REPS: 10,
+        MIN_WEIGHT: 1
+});
